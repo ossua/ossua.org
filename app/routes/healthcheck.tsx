e0d7ab46119eccdb6e62fb51c57d24ac1e3d9d32
Blue Mouse <durexp@protonmail.ch>
2024-06-02 13:00:11 +0100

    ignore healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -8,15 +8,17 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
   try {
-    const url = new URL("/", `http://${host}`);
-    // if we can connect to the database and make a simple query
-    // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      prisma.user.count(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
+    // We don't use a database yet, so this healthcheck can be ignored for now
+
+    // const url = new URL("/", `http://${host}`);
+    // // if we can connect to the database and make a simple query
+    // // and make a HEAD request to ourselves, then we're good.
+    // await Promise.all([
+    //   prisma.user.count(),
+    //   fetch(url.toString(), { method: "HEAD" }).then((r) => {
+    //     if (!r.ok) return Promise.reject(r);
+    //   }),
+    // ]);
     return new Response("OK");
   } catch (error: unknown) {
     console.log("healthcheck ❌", { error });
